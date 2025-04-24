6dd13ed9faee7d999be5612bf1ff5b8b6863c887
ahmad8743 <hamzehahmad06@gmail.com>
2025-04-23 22:54:48 -0500

    First few parts done

--- Part1.m
+++ Part1.m
@@ -1,16 +1,79 @@
-% Names: Ahmad Hamzeh, Samir Afsary
-% Case Study Two Part One
+%% Names: Ahmad Hamzeh, Samir Afsary
+%% Case Study Two Part One
 
+%% Solving our differential equation 
+clear; clc;
 R = 1000; % 1000 ohms
 C = 10^(-6); % 1 microfarad
 
 % Differential equation as layed out in the paper
 dq = @(t, q) (5 - 1 / C * q) / R;
 
-% Solve
-tspan = [0 0.5];
+% Solve differential equation numerically
+tspan = [0 0.05];
 q0 = 0;
-[t, sol] = ode45(dq, tspan, q0);
+[t1, sol] = ode15s(dq, tspan, q0); % Use ode15s for stiff equation
 
+% Analytical solution for the DE
+t2 = linspace(0, 0.05, 100);
+ansol = 5 * C * (1 - exp(-t2 /(R * C)));
+
+% Plot findings
+% Numerical solution
 figure(1);
-plot(t, sol);
+subplot(1, 2, 1);
+plot(t1, sol, 'LineWidth', 2);
+ylim([0, 5 * C + 10^(-7)]);
+title('Numerical solution');
+axis square;
+
+% Analytical solution
+subplot(1, 2, 2);
+plot(t2, ansol, 'LineWidth', 2);
+ylim([0, 5 * C + 10^(-7)]);
+title('Analytical solution');
+axis square;
+exportgraphics(gcf, 'Part1Plots/solcomp.png');
+
+%% Comparing changes in time constant
+clear; clc;
+R = 1000; % 1000 ohms
+C = 10^(-6); % 1 microfarad
+
+tau1 = R * C; % our original time constant
+% Change original by factor of 10 in each way
+tau2 = tau1 * 10;
+tau3 = tau1 / 10;
+
+% Solve for each tau
+% For simplicity, we will assume that capacitance isn't change and that the
+% resistance of the circuit is being modified
+t = linspace(0, 0.05, 100);
+sol1 = 5 * C * (1 - exp(-t /(tau1)));
+sol2 = 5 * C * (1 - exp(-t /(tau2)));
+sol3 = 5 * C * (1 - exp(-t /(tau3)));
+
+% Plot the solutions on the same graph to highlight differences
+figure(2);
+plot(t, sol1, 'LineWidth', 2);
+hold on;
+plot(t, sol2, 'LineWidth', 2);
+hold on;
+plot(t, sol3, 'LineWidth', 2);
+hold on;
+title('Graph of charge with varying time constants');
+legend({'Original', 'Scale up by 10', 'Scale down by 10'});
+exportgraphics(gcf, 'Part1Plots/tau.png');
+
+%% Plotting amplitude as a function of frequency
+clear; clc;
+R = 1000; % 1000 ohms
+C = 10^(-6); % 1 microfarad
+
+w = linspace(50, 1000, 1000); % As specified in the directions
+sol = 5 ./ sqrt(1 + (2 * pi * w * R * C).^2);
+
+figure(3);
+plot(w, sol, 'LineWidth', 2);
+title('Graph of amplitude as a fucntion of frequency');
+exportgraphics(gcf, 'Part1Plots/freq.png');
